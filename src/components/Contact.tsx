92d1c1218902a0169aee725880bb2fbbfadda8e1
Ahmed-ITCS <ahmedkhawarbs@gmail.com>
2025-06-16 07:38:12 +0300

    url

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,7 +18,7 @@ const Contact = () => {
     console.log('Form submitted:', formData);
     // Handle form submission here
     try {
-      const response = await fetch('https://portfolio-backend-brown-two.vercel.app/api/contact.js', { // Replace with your actual API endpoint
+      const response = await fetch('https://portfolio-backend-brown-two.vercel.app/api/contact.js', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
